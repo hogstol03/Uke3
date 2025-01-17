99e75a314a1125ee622b7331df3d2e96dbac7102
hogstol03 <juliabhogstol@hotmail.com>
2025-01-17 19:07:09 +0100

    La til getPoem

--- script.mjs
+++ script.mjs
@@ -11,8 +11,20 @@ function getRoot(req, res, next) {
     res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
 }
 
+function getPoem(req, res, next) {
+    const poem = `
+        Roses are red,
+        Violets are blue,
+        Sugar is sweet,
+        And so are you.
+    `;
+    res.status(HTTP_CODES.SUCCESS.OK).send(poem).end();
+}
+
 server.get("/", getRoot);
+server.get('/tmp/poem', getPoem);
 
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
 });
+
