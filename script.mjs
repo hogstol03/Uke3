d209b41b8fce202cb1a141084e78d1643a3d157d
hogstol03 <juliabhogstol@hotmail.com>
2025-01-17 19:09:12 +0100

    La til getQuote

--- script.mjs
+++ script.mjs
@@ -21,6 +21,19 @@ function getPoem(req, res, next) {
     res.status(HTTP_CODES.SUCCESS.OK).send(poem).end();
 }
 
+function getQuote(req, res, next) {
+    const quotes = [
+        "The only limit to our realization of tomorrow is our doubts of today. – Franklin D. Roosevelt",
+        "Do what you can, with what you have, where you are. – Theodore Roosevelt",
+        "In the middle of every difficulty lies opportunity. – Albert Einstein",
+        "Life is 10% what happens to us and 90% how we react to it. – Charles R. Swindoll",
+        "It does not matter how slowly you go as long as you do not stop. – Confucius"
+    ];
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    res.status(HTTP_CODES.SUCCESS.OK).send(randomQuote).end();
+}
+
+server.get('/tmp/quote', getQuote);
 server.get("/", getRoot);
 server.get('/tmp/poem', getPoem);
 
