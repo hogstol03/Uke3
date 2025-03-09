b5c608186ea60ebcfffa5399256955a48d961598
hogstol03 <juliabhogstol@hotmail.com>
2025-03-09 20:43:00 +0100

    Opprettet flashcards-tabell i databasen

---
+++ models/createTables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS flashcards (
+    id SERIAL PRIMARY KEY,
+    question TEXT NOT NULL,
+    answer TEXT NOT NULL,
+    category TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
